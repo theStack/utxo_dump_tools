36e7e6d781e126c12cfffcd6e35db69c2f2ad9ff
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2022-05-01 16:53:15 +0200

    frist attempt of actually calculating MuHash -- works on signet height 3! \o/

--- calc_utxo_db_muhash.go
+++ calc_utxo_db_muhash.go
@@ -1,150 +1,19 @@
 package main
 
 import (
-    "bufio"
+    "crypto/sha256"
     "database/sql"
     "encoding/binary"
+    "encoding/hex"
     "fmt"
     _ "github.com/mattn/go-sqlite3"
-    "io"
+    "golang.org/x/crypto/chacha20"
     "math/big"
-    "os"
-    "time"
+    //"time"
 )
 
-const verbose bool = false;
-var prime, _ = new(big.Int).SetString( // prime used for secp256k1
-    "fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f", 16)
-
-// Decompress pubkey by calculating y = sqrt(x^3 + 7) % p
-// and negating the result if necessary
-func decompressPubkey(pubkey_in []byte, pubkey_out []byte) bool {
-    if len(pubkey_in) != 33 {
-        panic("compressed pubkey must be 33 bytes long!")
-    }
-    if pubkey_in[0] != 0x02 && pubkey_in[0] != 0x03 {
-        panic("compressed pubkey must have even/odd tag of 0x02 or 0x03!")
-    }
-    if len(pubkey_out) != 65 {
-        panic("storage for uncompressed pubkey must be 65 bytes long!")
-    }
-
-    x := new(big.Int).SetBytes(pubkey_in[1:])
-    x2 := new(big.Int).Mul(x, x)
-    x2.Mod(x2, prime) // x^2 = (x * x) % p
-    x3 := new(big.Int).Mul(x2, x)
-    x3.Mod(x3, prime) // x^3 = (x^2 * x) % p
-    rhs := new(big.Int).Add(x3, big.NewInt(7))
-    rhs.Mod(rhs, prime) // rhs = (x^3 + 7) % p
-    y := new(big.Int).ModSqrt(rhs, prime) // y = sqrt(x^3 + 7) % p
-    if y == nil {
-        fmt.Printf("WARNING: Couldn't find modular square root!\n")
-        return false
-    }
-
-    tag_is_odd := pubkey_in[0] == 3
-    y_is_odd := y.Bit(0) == 1
-    if tag_is_odd != y_is_odd {
-        neg_y := new(big.Int).Sub(big.NewInt(0), y)
-        y.Mod(neg_y, prime)
-    }
-
-    pubkey_out[0] = 0x04
-    x.FillBytes(pubkey_out[1:33])
-    y.FillBytes(pubkey_out[33:65])
-    return true
-}
-
-func readIntoSlice(r *bufio.Reader, buf []byte) {
-    _, err := io.ReadFull(r, buf)
-    if err != nil { panic(err) }
-}
-
-func readUInt32(r *bufio.Reader, target *uint32) {
-    err := binary.Read(r, binary.LittleEndian, target)
-    if err != nil { panic(err) }
-}
-
-func readUInt64(r *bufio.Reader, target *uint64) {
-    err := binary.Read(r, binary.LittleEndian, target)
-    if err != nil { panic(err) }
-}
-
-func readCompressedScript(spkSize uint64, r *bufio.Reader) (bool, []byte) {
-    buf := make([]byte, 0, 67)  // enough capacity for special types (0-5)
-    switch spkSize {
-    case 0: // P2PKH
-        buf = buf[:25]
-        buf[0], buf[1], buf[2] = 0x76, 0xa9, 20
-        readIntoSlice(r, buf[3:23])
-        buf[23], buf[24] = 0x88, 0xac
-    case 1: // P2SH
-        buf = buf[:23]
-        buf[0], buf[1] = 0xa9, 20
-        readIntoSlice(r, buf[2:22])
-        buf[22] = 0x87
-    case 2, 3: // P2PK (compressed)
-        buf = buf[:35]
-        buf[0], buf[1] = 33, byte(spkSize)
-        readIntoSlice(r, buf[2:34])
-        buf[34] = 0xac
-    case 4, 5: // P2PK (uncompressed)
-        buf = buf[:67]
-        var compressed_pubkey [33]byte
-        compressed_pubkey[0] = byte(spkSize) - 2
-        readIntoSlice(r, compressed_pubkey[1:])
-        buf[0] = 65
-        success := decompressPubkey(compressed_pubkey[:], buf[1:66])
-        if !success {
-            return false, nil
-        }
-        buf[66] = 0xac
-    default: // others (bare multisig, segwit etc.)
-        readSize := spkSize - 6
-        if readSize > 10000 {
-            panic(fmt.Sprintf("too long script with size %d\n", readSize))
-        }
-        buf = make([]byte, readSize)
-        readIntoSlice(r, buf[:])
-    }
-
-    return true, buf
-}
-
-func readVARINT(r *bufio.Reader) (uint64) {
-    n := uint64(0)
-    for {
-        dat, _ := r.ReadByte()
-        n = (n << 7) | uint64(dat & 0x7f)
-        if (dat & 0x80) > 0 {
-            n++
-        } else {
-            return n
-        }
-    }
-}
-
-func decompressAmount(x uint64) (uint64) {
-    if x == 0 {
-        return 0
-    }
-    x--
-    e := x % 10
-    x /= 10
-    n := uint64(0)
-    if e < 9 {
-        d := (x % 9) + 1
-        x /= 9
-        n = x*10 + d
-    } else {
-        n = x+1
-    }
-    for e > 0 {
-        n *= 10
-        e--
-    }
-    return n
-}
+const verbose bool = true;
+var num3072_prime = new(big.Int).Sub(new(big.Int).Lsh(big.NewInt(1), 3072), big.NewInt(1103717))
 
 func hashToStr(bytes [32]byte) (string) {
     for i, j := 0, 31; i < j; i, j = i+1, j-1 {
@@ -153,90 +22,110 @@ func hashToStr(bytes [32]byte) (string) {
     return fmt.Sprintf("%x", bytes)
 }
 
-func execStmt(db *sql.DB, stmt string) {
-    _, err := db.Exec(stmt)
-    if err != nil { panic(err) }
-}
-
 func main() {
-    homeDir, err := os.UserHomeDir()
-    f, err := os.OpenFile(homeDir + "/.bitcoin/utxo.dat", os.O_RDONLY, 0600)
-    if err != nil { panic(err) }
-    utxof := bufio.NewReader(f)
-
-    // read metadata
-    var blockHash [32]byte
-    var numUTXOs uint64
-    readIntoSlice(utxof, blockHash[:])
-    readUInt64(utxof, &numUTXOs)
-    fmt.Printf("UTXO Snapshot at block %s, contains %d coins\n",
-               hashToStr(blockHash), numUTXOs)
-
-    db, err := sql.Open("sqlite3", "file:utxos.sqlite3?_journal_mode=off")
+    db, err := sql.Open("sqlite3", "file:/home/honeybadger/.bitcoin/signet/utxo.sqlite")
     if err != nil { panic(err) }
     defer db.Close()
 
-    execStmt(db, "DROP TABLE IF EXISTS utxos")
-    execStmt(db, "CREATE TABLE utxos (prevoutHash BLOB, prevoutIndex INT, blockHeight INT, isCoinbase INT, scriptPubKey BLOB, amount INT)")
-    addUTXOStmt, err := db.Prepare("INSERT INTO utxos VALUES (?, ?, ?, ?, ?, ?)")
+    // TODO: read metadata
+    //fmt.Printf("UTXO Snapshot at block %s, contains %d coins\n",
+    //           hashToStr(blockHash), numUTXOs)
+
+    rows, err := db.Query("SELECT * FROM utxos")
     if err != nil { panic(err) }
-    defer addUTXOStmt.Close()
-    tx, err := db.Begin()
-    if err != nil { panic(err) }
+    defer rows.Close()
 
-    t := time.Now()
+    //t := time.Now()
 
-    coins_skipped := uint64(0)
-    for coin_idx := uint64(1); coin_idx <= numUTXOs; coin_idx++ {
-        // read key (COutPoint)
-        var prevoutHash [32]byte
-        var prevoutIndex uint32
-        readIntoSlice(utxof, prevoutHash[:])
-        readUInt32(utxof, &prevoutIndex)
+    num3072 := big.NewInt(1)
 
-        // read value (Coin)
-        code := readVARINT(utxof)
-        height := code >> 1
-        isCoinbase := code & 1
-        amount := decompressAmount(readVARINT(utxof))
-        spkSize := readVARINT(utxof)
-        success, scriptPubKey := readCompressedScript(spkSize, utxof)
+    for rows.Next() {
+        var txid_hex string
+        var vout uint64
+        var value uint64
+        var coinbase uint64
+        var height uint64
+        var scriptpubkey_hex string
 
-        // write to database
-        if success {
-            _, err = tx.Stmt(addUTXOStmt).Exec(prevoutHash[:], prevoutIndex, height, isCoinbase, scriptPubKey, amount)
-            if err != nil { panic(err) }
-        } else {
-            coins_skipped++
+        err = rows.Scan(&txid_hex, &vout, &value, &coinbase, &height, &scriptpubkey_hex)
+        if err != nil { panic(err) }
+
+        txid, err := hex.DecodeString(txid_hex)
+        if err != nil { panic(err) }
+        for i, j := 0, 31; i < j; i, j = i+1, j-1 {
+            txid[i], txid[j] = txid[j], txid[i]
         }
+        scriptpubkey, err := hex.DecodeString(scriptpubkey_hex)
+        if err != nil { panic(err) }
 
         if verbose {
-            fmt.Printf("Coin %d/%d:\n", coin_idx, numUTXOs)
-            fmt.Printf("\tprevout.hash = %s\n", hashToStr(prevoutHash))
-            fmt.Printf("\tprevout.n = %d\n", prevoutIndex)
-            fmt.Printf("\theight = %d, is_coinbase = %d\n", height, isCoinbase)
-            fmt.Printf("\tamount = %d sats\n", amount)
-            fmt.Printf("\tscriptPubKey = %x\n", scriptPubKey)
+            fmt.Printf("\ttxid = %x\n", txid)
+            fmt.Printf("\tvout = %d\n", vout)
+            fmt.Printf("\tvalue = %d sats\n", value)
+            fmt.Printf("\tcoinbase = %d\n", coinbase)
+            fmt.Printf("\theight = %d\n", height)
+            fmt.Printf("\tscriptPubKey = %x\n", scriptpubkey)
+            fmt.Printf("\n")
         }
 
-        if coin_idx % (1024*1024) == 0 {
-            elapsed := time.Since(t)
-            fmt.Printf("%d coins read [%.2f%%], %d coins skipped, %s passed since start\n",
-                coin_idx, (float32(coin_idx)/float32(numUTXOs))*100, coins_skipped, elapsed)
-            tx.Commit()
-            tx, err = db.Begin()
-            if err != nil { panic(err) }
-        }
-    }
-    tx.Commit()
+        //////////////////////////////////////
+        // serialize to be ready for muhash
+        //////////////////////////////////////
+        muser := make([]byte, 0, 128)
+        muser = append(muser, txid...)
 
-    // check for EOF (read must fail)
-    _, err = utxof.ReadByte()
-    if err != nil {
-        fmt.Println("EOF reached.")
-    } else {
-        fmt.Println("WARNING: File is not at EOF yet!")
+        tmp4 := make([]byte, 4)
+        tmp8 := make([]byte, 8)
+        binary.LittleEndian.PutUint32(tmp4, uint32(vout))
+        muser = append(muser, tmp4...)
+
+        binary.LittleEndian.PutUint32(tmp4, 2*uint32(height) + uint32(coinbase))
+        muser = append(muser, tmp4...)
+
+        binary.LittleEndian.PutUint64(tmp8, value)
+        muser = append(muser, tmp8...)
+
+        // TODO: also handle larger pubkeyscript-sizes (compact size...)
+        if len(scriptpubkey) > 250 {
+            panic("TODO: implement compact size serialization, len of scriptPubKey is too long...")
+        }
+        muser = append(muser, byte(len(scriptpubkey)))
+        muser = append(muser, scriptpubkey...)
+
+        fmt.Printf("FIRST UTXO for muhash, serialized: %x\n", muser)
+
+        ///////////////// next step, hash of that thing /////////////////
+        hashed := sha256.Sum256(muser)
+        fmt.Printf("SHA256 of the serialized UTXO: %x\n", hashed)
+
+        //// chacha20 ////
+        nonce := [12]byte{0,0,0,0,0,0,0,0,0,0,0,0}
+        cc20, err := chacha20.NewUnauthenticatedCipher(hashed[:], nonce[:])
+        if err != nil { panic(err) }
+        var num3072_raw [384]byte
+        cc20.XORKeyStream(num3072_raw[:], num3072_raw[:])
+        fmt.Printf("Chacha20 of SHA256 of the serialized UTXO: %x\n", num3072_raw)
+
+        for i, j := 0, 383; i < j; i, j = i+1, j-1 {
+            num3072_raw[i], num3072_raw[j] = num3072_raw[j], num3072_raw[i]
+        }
+
+        num3072_insert := new(big.Int).SetBytes(num3072_raw[:])
+        num3072_insert.Mod(num3072_insert, num3072_prime)
+
+        num3072.Mul(num3072, num3072_insert)
+        num3072.Mod(num3072, num3072_prime)
+
+        var xxx [384]byte
+        num3072.FillBytes(xxx[:])
+        for _, b := range xxx {
+            fmt.Printf("%02x", b)
+        }
+        for i, j := 0, 383; i < j; i, j = i+1, j-1 {
+            xxx[i], xxx[j] = xxx[j], xxx[i]
+        }
+        fmt.Printf("\n")
+        hashed = sha256.Sum256(xxx[:])
+        fmt.Printf("Final SHA256 of the Num3072 (MuHash): %s\n", hashToStr(hashed))
     }
-    fmt.Printf("TOTAL: %d coins read, %d coins skiped => %d coins written.\n",
-        numUTXOs, coins_skipped, numUTXOs - coins_skipped)
 }
--- utxo_to_sqlite/go.mod
+++ utxo_to_sqlite/go.mod
@@ -2,4 +2,9 @@ module utxo_to_sqlite
 
 go 1.17
 
-require github.com/mattn/go-sqlite3 v1.14.12
+require (
+	github.com/mattn/go-sqlite3 v1.14.12
+	golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f
+)
+
+require golang.org/x/sys v0.0.0-20220422013727-9388b58f7150 // indirect
